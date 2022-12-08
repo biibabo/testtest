873d54357d088d193748f8ffabe55ca7ecf91bf8
junbo.zhao <junbo.zhao@ucloud.cn>
2022-12-08 19:35:43 +0800

    Changes

--- testcase/test_ddt_firewall/test_eip.py
+++ testcase/test_ddt_firewall/test_eip.py
@@ -7,8 +7,10 @@ from core.basecase import BaseCase
 
 @ddt
 class TestEip(BaseCase):
+    global resourceid
     @file_data("../../testdata/test_eip/test_eip_ucloud.json")
     def test_createucloudeip(self, EIPInstance, expect_code):
+        global resourceid
         url = "https://cmp-fe.ucloud.cn/api/gateway?Action=CreateEIP"
         data = {"OrgId": 1,
                 "AccountId": 2,
@@ -25,4 +27,15 @@ class TestEip(BaseCase):
         print("{:-^100s}".format("Split Line"))
         # print(r.json())
         self.assertEqual(expect_code, r.json()['RetCode'])
-        # assert r.json()['RetCode'] == expect_code
+        resourceid = r.json()["Data"]["ResourceId"]
+
+    def test_delucloudeip(self):
+        url = "https://cmp-fe.ucloud.cn/api/gateway?Action=DeleteEIP"
+        data = {"ResourceIds": [resourceid]}
+        header = {
+            'Referer': 'https://cmp-fe.ucloud.cn/cloud-fe/resource/network/eip',
+            'Origin': 'https://cmp-fe.ucloud.cn',
+            'Cookie': self.cookie}
+
+        r = self.request(method="post", url=url, json=data, headers=header)
+        print(r.json())
